d0a000ba181678b53854ae7b63356a05aeb6af9d
Kristian Tot <k.tot@vegait.rs>
2022-10-15 00:38:32 +0200

    Fix errors

--- backend/src/apps/users/models/abstract_profile.py
+++ backend/src/apps/users/models/abstract_profile.py
@@ -1,12 +1,11 @@
-from apps.caregivers.models import BaseModel
-
+from src.caregivers.models import BaseModel
+from django.db import models
 
 
 class AbstractUser(BaseModel):
-    profile_image=models.TextField()
-    gender=models.CharField() # pogledaj za enum 
-    description=models.TextField()
-    type_user=models.TextField() #caregivers && beneficial profile 
-    postal_code=models.IntegerField()  
-    city=models.CharField() 
-
+    profile_image = models.TextField()
+    gender = models.CharField()  # pogledaj za enum
+    description = models.TextField()
+    type_user = models.TextField()  # caregivers && beneficial profile
+    postal_code = models.IntegerField()
+    city = models.CharField()
--- backend/src/caregivers/models.py
+++ backend/src/caregivers/models.py
@@ -1,9 +1,10 @@
 from django_extensions.db.models import TimeStampedModel
 
+
+
 class BaseModel(TimeStampedModel):
     class Meta:
         abstract = True
-
 
     def update(self, **kwargs):
         for field_name, field_value in kwargs.items():
