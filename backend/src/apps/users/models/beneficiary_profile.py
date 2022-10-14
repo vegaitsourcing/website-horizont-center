335f4aaed9439dfd997d7c86a145dd9c41ae9d70
Kristian Tot <k.tot@vegait.rs>
2022-10-15 00:40:05 +0200

    Fix errors

--- backend/src/apps/users/models/beneficiary_profile.py
+++ backend/src/apps/users/models/beneficiary_profile.py
@@ -1,9 +1,10 @@
-from apps.caregivers.models import BaseModel
+from src.caregivers.models import BaseModel
+from django.db import models
 
-class Beneficiary(BaseModel): 
-    beneficiary_person=models.CharField() 
-    helping_period=models.CharField()
-    weekly_days=models.IntegerField()
-    daily_hours=models.FloatFiled()
-    care_type=models.CharField()
-    
+
+class Beneficiary(BaseModel):
+    beneficiary_person = models.CharField()
+    helping_period = models.CharField()
+    weekly_days = models.IntegerField()
+    daily_hours = models.FloatFiled()
+    care_type = models.CharField()
