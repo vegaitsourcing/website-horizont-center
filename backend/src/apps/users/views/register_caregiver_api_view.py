70a8c0504c0b9a38c1abc3d323ee00fb28038865
Kristian Tot <k.tot@vegait.rs>
2022-10-16 01:20:58 +0200

    Register api view

--- backend/src/apps/users/views/register_caregiver_api_view.py
+++ backend/src/apps/users/views/register_caregiver_api_view.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from apps.users.models import User, CaregiverProfile
 from apps.users.serializers import CaregiverUserSerializer
+from caregivers.utils.send_verification_token_email_notification import SendVerificationTokenEmailNotification
+from caregivers.utils.verification_token import VerificationToken
 
 
 class RegisterCaregiverAPIView(APIView):
@@ -13,5 +15,11 @@ class RegisterCaregiverAPIView(APIView):
             caregiver_profile_kwargs = caregiver_serializer.validated_data.pop('caregiver_profile')
             user = User.objects.create_user(**caregiver_serializer.validated_data)
             CaregiverProfile.objects.create(user=user, **caregiver_profile_kwargs)
+
+            # TODO send email with generate hash
+            url_hash = VerificationToken.generate_url_hash_from_email(user.email)
+            send_verification_token_email_notification = SendVerificationTokenEmailNotification(url_hash, user.email)
+            send_verification_token_email_notification.try_to_send_email()
             return JsonResponse(data={'message': 'success'})
         return JsonResponse(data={'errors': caregiver_serializer.errors}, status=BAD_REQUEST)
+
