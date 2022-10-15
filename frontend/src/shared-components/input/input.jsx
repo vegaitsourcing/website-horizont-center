eaa1492bf7ad07b3f921403a2316f8922cdeb68c
Neamanja Porobic <nemanja.porobic1@howdengroup.com>
2022-10-15 23:01:56 +0200

    fix issues with input css style

--- frontend/src/shared-components/input/input.jsx
+++ frontend/src/shared-components/input/input.jsx
@@ -8,30 +8,34 @@ export const Input = ({ id, name, type, placeholder, hasError, options, required
   if (type == "dropdown")
     return (
       <>
-        <select
-          name={name}
-          id={id}
-          className={styles.input + " " + styles.dropdown + " " + (hasError ? styles.hasError : null)}
-        >
-          {placeholder != null ? <option value="">{placeholder}</option> : null}
-          {options.map((option) => {
-            return <option value={option}>{option}</option>;
-          })}
-        </select>
-        <FontAwesomeIcon icon={faChevronDown} className={styles.icon} />
+        <div className={styles.inputWrapper}>
+          <select
+            name={name}
+            id={id}
+            className={styles.input + " " + styles.dropdown + " " + (hasError ? styles.hasError : null)}
+          >
+            {placeholder != null ? <option value="">{placeholder}</option> : null}
+            {options.map((option) => {
+              return <option value={option}>{option}</option>;
+            })}
+          </select>
+          <FontAwesomeIcon icon={faChevronDown} className={styles.icon} />
+        </div>
       </>
     );
   else if (type == "search") {
     return (
       <>
-        <input
-          type={type}
-          name={name}
-          id={id}
-          placeholder={placeholder}
-          className={styles.input + " " + (hasError ? styles.hasError : null)}
-        />
-        <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} />
+        <div className={styles.inputWrapper}>
+          <input
+            type={type}
+            name={name}
+            id={id}
+            placeholder={placeholder}
+            className={styles.input + " " + (hasError ? styles.hasError : null)}
+          />
+          <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} />
+        </div>
       </>
     );
   } else {
