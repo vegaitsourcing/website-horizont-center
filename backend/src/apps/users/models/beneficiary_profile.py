e1eedd7aaf257e46590a9b0bcfaa5c705e28e930
Kristian Tot <k.tot@vegait.rs>
2022-10-15 11:12:02 +0200

    Beneficiary profile model updated

--- backend/src/apps/users/models/beneficiary_profile.py
+++ backend/src/apps/users/models/beneficiary_profile.py
@@ -1,10 +1,24 @@
-from src.caregivers.models import BaseModel
+from src.apps.users.models.abstract_profile import AbstractProfile
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class Beneficiary(BaseModel):
-    beneficiary_person = models.CharField()
-    helping_period = models.CharField()
+class BeneficiaryProfile(AbstractProfile):
+    class Meta:
+        verbose_name = _('Beneficiary profile')
+        verbose_name_plural = _('Beneficiary profiles')
+
+    beneficiary_person = models.CharField(
+        verbose_name=_('beneficiary person'),
+        max_length=250
+    )
+    helping_period = models.CharField(
+        verbose_name=_('helping period'),
+        max_length=250
+    )
     weekly_days = models.IntegerField()
-    daily_hours = models.FloatFiled()
-    care_type = models.CharField()
+    daily_hours = models.IntegerField()
+    care_type = models.CharField(
+        max_length=250,
+        verbose_name=_('care type')
+    )
