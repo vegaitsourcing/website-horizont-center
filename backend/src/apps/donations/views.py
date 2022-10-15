aedbd18ba2845ba2103a5d241153517b59d1b683
aco4vegait <aleksandar.brkljac@vegait.rs>
2022-10-15 22:19:16 +0200

    feat: donations pagination

--- backend/src/apps/donations/views.py
+++ backend/src/apps/donations/views.py
@@ -1,15 +1,23 @@
+from django.http import HttpRequest
 from django.http.response import JsonResponse
+from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from apps.donations.models import Donation
 from apps.donations.serializers.donations_serializer import DonationsSerializer
+from caregivers.serializers.list_response_serializer import ListResponseSerializer
 
 
 class DonationAPIView(APIView):
     @staticmethod
-    def get(request) -> JsonResponse:
+    @api_view(["GET"])
+    def get(request: HttpRequest) -> JsonResponse:
         donations = Donation.objects.all()
-        serializer = DonationsSerializer(donations, many=True)
+        serializer = ListResponseSerializer(
+            queryset=donations,
+            model_serializer_class=DonationsSerializer,
+            request=request,
+        )
         return JsonResponse(data=serializer.data, safe=False)
 
     @staticmethod
