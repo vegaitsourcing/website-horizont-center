99e2380842b18c4454673b1cfff99552fe6165ad
Kristian Tot <k.tot@vegait.rs>
2022-10-15 13:43:41 +0200

    Beneficiary profile admin page created

---
+++ backend/src/apps/users/admin/beneficiary_profile_admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from apps.users.models import BeneficiaryProfile
+
+
+@admin.register(BeneficiaryProfile)
+class BeneficiaryProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'beneficiary_person',
+        'helping_period',
+        'weekly_days',
+        'daily_hours',
+        'care_type',
+    )
+
+    search_fields = ('beneficiary_person',
+                     'care_type',
+                     )
+
+    ordering = ('care_type',)
