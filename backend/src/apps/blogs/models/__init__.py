ad38d84a9068f5189e144438748009d10442bcb7
Kristian Tot <k.tot@vegait.rs>
2022-10-15 16:44:17 +0200

    add rel 1)blog->blog category 2)blog->blog section 3)blog->blog author

--- backend/src/apps/blogs/models/__init__.py
+++ backend/src/apps/blogs/models/__init__.py
@@ -1,3 +1,4 @@
 from apps.blogs.models.blog import Blog
-
-
+from apps.blogs.models.blog_category import BlogCategory
+from apps.blogs.models.blog_section import BlogSection
+from apps.blogs.models.blog_author import BlogAuthor
