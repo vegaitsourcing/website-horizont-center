64379c44bee4307c3b82abdf4f8f640ad3126ca9
Kristian Tot <k.tot@vegait.rs>
2022-10-16 01:20:08 +0200

    Register new route

--- backend/src/apps/users/urls.py
+++ backend/src/apps/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.users.views import RegisterCaregiverAPIView, RegisterBeneficiaryAPIView
+
+from apps.users.views import RegisterCaregiverAPIView, RegisterBeneficiaryAPIView, RegisterAPIView
 
 api_url_patterns = [
     path('register-caregiver/', RegisterCaregiverAPIView.as_view(), name='register_caregiver'),
     path('register-beneficiary/', RegisterBeneficiaryAPIView.as_view(), name='register_beneficiary'),
+    path('register/<str:token>', RegisterAPIView.as_view(), name='register'),
 ]
