cfeaf1c8cadf28b39a0a2c943e8ad16a2dca0e1a
Kristian Tot <k.tot@vegait.rs>
2022-10-15 19:26:30 +0200

    init.py import changes

--- backend/src/apps/blogs/admin/__init__.py
+++ backend/src/apps/blogs/admin/__init__.py
@@ -1,4 +1,4 @@
 from apps.blogs.admin.blog_admin import BlogAdmin
 from apps.blogs.admin.blog_category_admin import BlogCategoryAdmin
-from apps.blogs.admin.blog_author_admin_inline import BlogAuthorSectionAdmin
-from apps.blogs.admin.blog_section_admin_inline import BlogSectionAdmin
+from apps.blogs.admin.blog_author_admin_inline import BlogAuthorAdminInline
+from apps.blogs.admin.blog_section_admin_inline import BlogSectionAdminInline
