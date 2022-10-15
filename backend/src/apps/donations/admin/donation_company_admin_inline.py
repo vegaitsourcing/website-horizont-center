bc5741eb301555ea93e3f37be7bde83f96150381
aco4vegait <aleksandar.brkljac@vegait.rs>
2022-10-15 16:49:45 +0200

    fix: Migration bug, company inline to stack and translate

--- backend/src/apps/donations/admin/donation_company_admin_inline.py
+++ backend/src/apps/donations/admin/donation_company_admin_inline.py
@@ -3,9 +3,8 @@ from django.contrib import admin
 from apps.donations.models import DonationCompany
 
 
-class DonationCompanyForm(admin.TabularInline):
+class DonationCompanyForm(admin.StackedInline):
     model = DonationCompany
     extra = 0
     min_num = 1
     max_num = 1
-    can_delete = False
