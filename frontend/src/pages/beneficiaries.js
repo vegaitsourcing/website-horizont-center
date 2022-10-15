688e64c4f1827a626a1b7e5d1dcd3f849b84396a
Marko Puzovic <mpuzovic@suitsupply.com>
2022-10-15 15:42:36 +0200

    user profile item and list of caregivers and beneficiaries

--- frontend/src/pages/beneficiaries.js
+++ frontend/src/pages/beneficiaries.js
@@ -3,13 +3,27 @@ import { LayoutDefault } from "layouts";
 import { NextSeo } from "next-seo";
 
 import ENV from "config/env";
+import beneficiariesService from "./api/beneficiariesService";
+import { ProfileListItem } from "shared-components/profile-list-item/profile-list-item";
 
 const { BASE_URL = "", BASE_API_URL = "", BASE_SEO = "", STATIC_DIR = "", AUTHOR } = ENV;
 
 function GettingStarted(props) {
   const {
     pathname,
-    data: { title, metaTitle, description, metaDescription, slug, services, block_top = {} },
+    data: {
+      title,
+      results,
+      pageNumber,
+      pageSize,
+      total,
+      metaTitle,
+      description,
+      metaDescription,
+      slug,
+      services,
+      block_top = {},
+    },
   } = props;
 
   const SEOS = {
@@ -29,16 +43,30 @@ function GettingStarted(props) {
   return (
     <>
       <NextSeo {...SEOS} />
-      <LayoutDefault>Stranica negovani...</LayoutDefault>
+      <LayoutDefault>
+        {results.map(({ id, care_type, image, created, city, description, first_name, last_name, helping_period }) => (
+          <ProfileListItem
+            body={description}
+            city={city}
+            createdDate={created}
+            firstName={first_name}
+            image={image}
+            lastName={last_name}
+            title={care_type}
+            key={id}
+            url={`${pathname}/${id}`}
+            period={helping_period}
+          ></ProfileListItem>
+        ))}
+      </LayoutDefault>
     </>
   );
 }
 
 export async function getServerSideProps(ctx) {
   const { resolvedUrl } = ctx;
-  const res = await fetch(`${BASE_API_URL}/api/global`);
-  const json = await res.json();
-  return { props: { data: json, pathname: resolvedUrl } };
+  const responseData = await beneficiariesService.getAllBeneficiaries();
+  return { props: { data: responseData, pathname: resolvedUrl } };
 }
 
 export default GettingStarted;
