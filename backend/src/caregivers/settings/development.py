466d25a0140b7d850ac92c53e7effb9db7219892
Kristian Tot <k.tot@vegait.rs>
2022-10-15 20:54:36 +0200

    Extend example.env with email host user and email host password Add email configuration to development.py

--- backend/src/caregivers/settings/development.py
+++ backend/src/caregivers/settings/development.py
@@ -1,4 +1,5 @@
 # Python imports
+import os
 from os.path import join
 
 # project imports
@@ -18,3 +19,10 @@ LOGOUT_REDIRECT_URL = 'core_login'
 # ##### APPLICATION CONFIGURATION #########################
 
 INSTALLED_APPS = DEFAULT_APPS
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
