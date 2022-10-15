5e8a23f5944c6a7be15dd561fbc6e88358391be3
Kristian Tot <k.tot@vegait.rs>
2022-10-15 12:55:33 +0200

    Add search and ordering to Caregiver profile admin view

--- backend/src/apps/users/admin/caregiver_profile_admin.py
+++ backend/src/apps/users/admin/caregiver_profile_admin.py
@@ -5,10 +5,13 @@ from apps.users.models.caregiver_profile import CaregiverProfile
 @admin.register(CaregiverProfile)
 class CaregiverProfileAdmin(admin.ModelAdmin):
     list_display = (
+        # Add first name and last name
         'birthdate',
         'city',
         'weekly_days',
         'daily_hours',
         'work_application',
-        'first_name'
     )
+
+    search_fields = ('city', 'work_application',)
+    ordering = ('city',)
