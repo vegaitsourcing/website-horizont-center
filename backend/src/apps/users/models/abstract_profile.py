8973dd004c083224557bb62af5930096c827c677
Kristian Tot <k.tot@vegait.rs>
2022-10-15 10:50:31 +0200

    Implemented abstract profile model

--- backend/src/apps/users/models/abstract_profile.py
+++ backend/src/apps/users/models/abstract_profile.py
@@ -1,11 +1,41 @@
 from src.caregivers.models import BaseModel
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.utils.translation import gettext_lazy as _
 
 
-class AbstractUser(BaseModel):
-    profile_image = models.TextField()
-    gender = models.CharField()  # pogledaj za enum
-    description = models.TextField()
-    type_user = models.TextField()  # caregivers && beneficial profile
-    postal_code = models.IntegerField()
-    city = models.CharField()
+class AbstractProfile(BaseModel):
+    class Gender(models.TextChoices):
+        MALE = 'MALE', _('Male')
+        FEMALE = 'FEMALE', _('Female')
+
+    profile_image = models.ImageField(
+        upload_to='images/users',
+        null=True,
+        blank=True,
+        verbose_name=_('profile image'),
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=Gender.choices,
+        verbose_name=_('gender'),
+    ),
+    postal_code = models.IntegerField(
+        verbose_name=_('postal code')
+    ),
+    city = models.CharField(
+        max_length=250,
+        verbose_name=_('city')
+    )
+    description = models.TextField(
+        validators=[
+            MinLengthValidator(limit_value=100)
+        ],
+        verbose_name=_('description'),
+    )
+
+    user = models.ForeignKey(
+        to='users.User',
+        verbose_name=_('user'),
+        on_delete=models.CASCADE,
+    )
