d0b238e41518183f84a0e4b568ec0994ba71fa7c
Kristian Tot <k.tot@vegait.rs>
2022-10-15 16:44:01 +0200

    add rel 1)blog->blog category 2)blog->blog section 3)blog->blog author

--- backend/src/apps/blogs/models/blog.py
+++ backend/src/apps/blogs/models/blog.py
@@ -9,15 +9,35 @@ class Blog(BaseModel):
         verbose_name_plural = _('Blogs')
 
         title = models.CharField(
-            verbose_name = _('title'),
-            max_length = 100
+            verbose_name=_('title'),
+            max_length=100
         )
 
         image = models.ImageField(
-            verbose_name =_('image'),
-            upload_to = _('images/blogs'),
+            verbose_name=_('image'),
+            upload_to=_('images/blogs'),
             null=True,
             blank=True,
         )
 
-        
+        blog_author = models.OneToOneField(
+            to='blogs.BlogAuthor',
+            verbose_name=_('blog author'),
+            on_delete=models.CASCADE,
+            unique=True,
+        )
+
+        blog_category = models.ForeignKey(
+            to='blogs.BlogCategory',
+            verbose_name=_('blog category'),
+            on_delete=models.CASCADE,
+            unique=True,
+        )
+
+        blog_section = models.ForeignKey(
+            to='blogs.BlogSection',
+            verbose_name=_('blog section'),
+            on_delete=models.CASCADE,
+            unique=True,
+        )
+
