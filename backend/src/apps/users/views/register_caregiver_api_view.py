c6ec5aa8f03851283c8653e32b373f0636d2d0c3
Kristian Tot <k.tot@vegait.rs>
2022-10-16 01:43:15 +0200

    update caregiver api view to send verification emails

--- backend/src/apps/users/views/register_caregiver_api_view.py
+++ backend/src/apps/users/views/register_caregiver_api_view.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from apps.users.models import User, CaregiverProfile
 from apps.users.serializers import CaregiverUserSerializer
+from apps.users.utils import create_caregiver_user, send_verification_email
 from caregivers.utils.send_verification_token_email_notification import SendVerificationTokenEmailNotification
 from caregivers.utils.verification_token import VerificationToken
 
@@ -12,14 +13,9 @@ class RegisterCaregiverAPIView(APIView):
     def post(self, request, **kwargs) -> JsonResponse:
         caregiver_serializer = CaregiverUserSerializer(data=request.data)
         if caregiver_serializer.is_valid():
-            caregiver_profile_kwargs = caregiver_serializer.validated_data.pop('caregiver_profile')
-            user = User.objects.create_user(**caregiver_serializer.validated_data)
-            CaregiverProfile.objects.create(user=user, **caregiver_profile_kwargs)
+            user = create_caregiver_user(caregiver_serializer)
+            send_verification_email(user)
 
-            # TODO send email with generate hash
-            url_hash = VerificationToken.generate_url_hash_from_email(user.email)
-            send_verification_token_email_notification = SendVerificationTokenEmailNotification(url_hash, user.email)
-            send_verification_token_email_notification.try_to_send_email()
             return JsonResponse(data={'message': 'success'})
-        return JsonResponse(data={'errors': caregiver_serializer.errors}, status=BAD_REQUEST)
 
+        return JsonResponse(data={'errors': caregiver_serializer.errors}, status=BAD_REQUEST)
