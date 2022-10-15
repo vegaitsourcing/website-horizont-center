63e1df1560aaf8326bb43268540caba7eb3ffdf9
Dusan Lukic <dusanlukic404@gmail.com>
2022-10-15 11:52:04 +0200

    created folder for create-account component

--- frontend/src/shared-components/index.jsx
+++ frontend/src/shared-components/index.jsx
@@ -1,3 +1,4 @@
 export * from "shared-components/input/input";
 export * from "shared-components/long-button/long.button";
 export * from "shared-components/short-button/short.button";
+export * from 'shared-components/create-account/create.account'
