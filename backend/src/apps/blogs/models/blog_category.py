831a95e308282faf12a251d1806ac783d9494278
Kristian Tot <k.tot@vegait.rs>
2022-10-15 15:25:10 +0200

    Blog category completed

---
+++ backend/src/apps/blogs/models/blog_category.py
@@ -0,0 +1,19 @@
+from caregivers.models import BaseModel
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+
+
+class BlogCategory(BaseModel):
+    class Meta:
+        verbose_name = _('Blog Category')
+        verbose_name_plural = ('Blog Categories')
+
+        name = models.CharField(
+            verbose_name=_('name'),
+            max_length=100
+        )
+
+        color = models.CharField(
+            verbose_name=_('color'),
+            max_length=100
+        )
