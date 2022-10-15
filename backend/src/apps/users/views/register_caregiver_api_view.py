86f84269b18d8c762d2fb39fdd497c1752727b3d
Kristian Tot <k.tot@vegait.rs>
2022-10-16 01:43:45 +0200

    Remove not needed imports

--- backend/src/apps/users/views/register_caregiver_api_view.py
+++ backend/src/apps/users/views/register_caregiver_api_view.py
@@ -1,11 +1,8 @@
 from http.client import BAD_REQUEST
 from django.http import JsonResponse
 from rest_framework.views import APIView
-from apps.users.models import User, CaregiverProfile
 from apps.users.serializers import CaregiverUserSerializer
 from apps.users.utils import create_caregiver_user, send_verification_email
-from caregivers.utils.send_verification_token_email_notification import SendVerificationTokenEmailNotification
-from caregivers.utils.verification_token import VerificationToken
 
 
 class RegisterCaregiverAPIView(APIView):
