dbd831cd93d8e031f409e7ba52cf7252e0685041
Milos Roknic <roknic.milos.994@gmail.com>
2022-10-27 11:03:07 +0200

    Refactor UserSerializer

--- backend/src/apps/users/serializers/user_serializer.py
+++ backend/src/apps/users/serializers/user_serializer.py
@@ -5,10 +5,10 @@ from apps.users.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = (
-            'id', 'password', 'last_login', 'is_superuser',
-            'is_staff', 'is_active', 'date_joined', 'created', 'modified',
-            'email', 'first_name', 'last_name', 'phone_number', 'second_phone_number',
+        exclude = (
+            'password',
+            'groups',
+            'user_permissions',
         )
 
     def get_sensitive_fields(self) -> tuple:
