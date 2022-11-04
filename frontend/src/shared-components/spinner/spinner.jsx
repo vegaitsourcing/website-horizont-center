5e9dbac092c3faec32548ccd8aa868b3048d2f8f
Milos Roknic <m.roknic@vegait.rs>
2022-11-04 15:25:01 +0100

    Implement Spinner component

---
+++ frontend/src/shared-components/spinner/spinner.jsx
@@ -0,0 +1,12 @@
+import styles from "./spinner.module.scss";
+
+export const Spinner = function () {
+	return (
+		<>
+			<div className={styles.spinnerBackground}>
+			</div>
+			<div className={styles.loadingContentLocation}></div>
+			<div className={styles.loader}></div>
+		</>
+	);
+};
