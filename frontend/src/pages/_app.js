6233e5c0ff21502e1b0165cbf7e39dda8db4a120
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-11-04 02:00:22 +0100

    add website icon via next/head

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -5,12 +5,14 @@ import "styles/main.scss";
 import { DefaultSeo, LocalBusinessJsonLd, SocialProfileJsonLd } from "next-seo";
 
 import SEO from "../../next-seo.config";
+import Head from "next/head";
 
 const { metas, microdatas } = SEO;
 
 export default function Application({ Component, pageProps }) {
   return (
     <>
+      <Head ><link rel="icon" href="caregivers_icon.svg" type="image/x-icon" /></Head>
       <DefaultSeo {...metas} />
       <LocalBusinessJsonLd {...microdatas.LocalBusinessJsonLd} />
       {/* <SocialProfileJsonLd {...microdatas.SocialProfileJsonLd} /> */}
