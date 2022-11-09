e65ff388fe1a45311668447f0e19d3cccbdc8bfc
Nemanja Porobic <n.porobic@vegait.rs>
2022-11-09 17:09:00 +0100

    fix styling z index on spinner

--- frontend/src/shared-components/spinner/spinner.jsx
+++ frontend/src/shared-components/spinner/spinner.jsx
@@ -4,7 +4,6 @@ export const Spinner = function () {
   return (
     <>
       <div className={styles.spinnerBackground}></div>
-      <div className={styles.loadingContentLocation}></div>
       <div className={styles.loader}></div>
     </>
   );
