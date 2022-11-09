82469efc20b870ce885e8f1423209aaaf4db31dc
Milos Roknic <m.roknic@vegait.rs>
2022-11-09 17:34:27 +0100

    Improve Blog Details

--- frontend/src/components/blog-details/blog.details.jsx
+++ frontend/src/components/blog-details/blog.details.jsx
@@ -1,32 +1,32 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import { BlogSections, AboutAuthor, PageHeader } from "shared-components";
+import { AboutAuthor, BlogSections, PageHeader } from "shared-components";
 import BlogsService from "pages/api/blogsService";
 
 export const BlogDetails = ({ blogId }) => {
-  const [isLoadingBlog, setIsLoadingBlog] = useState(true);
-  const [blog, setBlog] = useState(null);
+	const [isLoadingBlog, setIsLoadingBlog] = useState(true);
+	const [blog, setBlog] = useState(null);
 
-  async function getBlog() {
-    await BlogsService.getBlogById(blogId).then((response) => {
-      setBlog(response.data);
-      setIsLoadingBlog(false);
-    });
-  }
+	useEffect(() => {
+		async function getBlog() {
+			await BlogsService.getBlogById(blogId).then((response) => {
+				setBlog(response.data);
+				setIsLoadingBlog(false);
+			});
+		}
 
-  useEffect(() => {
-    if (isLoadingBlog) {
-      getBlog();
-    }
-  }, [isLoadingBlog]);
+		if (isLoadingBlog) {
+			getBlog();
+		}
+	}, [blogId, isLoadingBlog]);
 
-  if (isLoadingBlog) return null;
+	if (isLoadingBlog) return null;
 
-  return (
-    <>
-      <PageHeader isNarrow title={blog.title} image={blog.image} />
-      <BlogSections sections={blog.sections} />
-      {blog.author ? <AboutAuthor author={blog.author} /> : null}
-    </>
-  );
+	return (
+		<>
+			<PageHeader isNarrow title={blog.title} image={blog.image}/>
+			<BlogSections sections={blog.sections}/>
+			{blog.author ? <AboutAuthor author={blog.author}/> : null}
+		</>
+	);
 };
