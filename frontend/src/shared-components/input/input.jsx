108a2c13149c35763794bd4e88bc989f1e174b3e
Milos Roknic <m.roknic@vegait.rs>
2022-11-10 14:52:19 +0100

    Fix filter fields and implement prepareSEOS function

--- frontend/src/shared-components/input/input.jsx
+++ frontend/src/shared-components/input/input.jsx
@@ -5,37 +5,37 @@ import { SelectDate } from "shared-components";
 
 import styles from "./input.module.scss";
 
-export const Input = (props) => {
-  const {
-    id,
-    name,
-    type,
-    step,
-    placeholder,
-    inputValue,
-    valueChangedHandler,
-    isValidInput = true,
-    withSearchIcon = false,
-  } = props;
+export const Input = ({
+	id,
+	name,
+	type,
+	step,
+	placeholder,
+	inputValue,
+	valueChangedHandler,
+	className,
+	isValidInput = true,
+	withSearchIcon = false,
+}) => {
 
-  if (type === "dropdown") return <Select {...props} />;
+	if (type === "dropdown") return <Select {...props} />;
 
-  if (type === "datepicker") return <SelectDate {...props} />;
+	if (type === "datepicker") return <SelectDate {...props} />;
 
-  return (
-    <div className={styles.fieldWrapper}>
-      <input
-        defaultValue={inputValue ?? ""}
-        type={type}
-        name={name}
-        id={id}
-        step={step}
-        onChange={(event) => valueChangedHandler(event.target.value)}
-        placeholder={placeholder}
-        className={[styles.field, !isValidInput ? styles.inputError : ""].join(" ")}
-      />
-      {withSearchIcon && <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon} />}
-      {!isValidInput ? <div className={styles.textError}>Morate popuniti polje</div> : ""}
-    </div>
-  );
+	return (
+		<div className={[styles.fieldWrapperWide, className].join(" ")}>
+			<input
+				defaultValue={inputValue ?? ""}
+				type={type}
+				name={name}
+				id={id}
+				step={step}
+				onChange={(event) => valueChangedHandler(event.target.value)}
+				placeholder={placeholder}
+				className={[styles.field, !isValidInput ? styles.inputError : ""].join(" ")}
+			/>
+			{withSearchIcon && <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.icon}/>}
+			{!isValidInput ? <div className={styles.textError}>Morate popuniti polje</div> : ""}
+		</div>
+	);
 };
