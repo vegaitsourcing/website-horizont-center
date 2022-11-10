c08d277185913685275c4bb104c3e357bd050b22
Milos Roknic <m.roknic@vegait.rs>
2022-11-10 14:52:19 +0100

    Fix filter fields and implement prepareSEOS function

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,3 +1,6 @@
+import { ENV } from "./config/env";
+import { navigationItems } from "./config/navigationItems";
+
 export function hex2rgba(hex, alpha = 1) {
 	const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
 	return `rgba(${r},${g},${b},${alpha})`;
@@ -10,5 +13,21 @@ export function createResourceCardSecondaryTag({ name, color }) {
 			color: color,
 			backgroundColor: hex2rgba(color, 0.2),
 		},
+	};
+}
+
+export function prepareSEOS(pathname) {
+	const navigationItem = navigationItems.filter(item => item.pathname === pathname);
+	return {
+		title: navigationItem.name,
+		canonical: `${ENV.BASE_URL}${pathname}`,
+		openGraph: [
+			{
+				url: ENV.BASE_URL,
+				images: { url: `${ENV.BASE_URL}${ENV.STATIC_DIR}logo.png` },
+				site_name: ENV.AUTHOR,
+			},
+		],
+		...ENV.BASE_SEO,
 	};
 }
