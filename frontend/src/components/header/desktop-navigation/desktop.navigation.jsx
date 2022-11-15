b2f7ab93dc052c0237b1b761ae88d0f8df171f6b
Milos Roknic <m.roknic@vegait.rs>
2022-11-15 10:57:30 +0100

    Improve navigation bar

--- frontend/src/components/header/desktop-navigation/desktop.navigation.jsx
+++ frontend/src/components/header/desktop-navigation/desktop.navigation.jsx
@@ -6,59 +6,63 @@ import { LogoButton } from "../logo-button/logo.button";
 import { navigationItems } from "../../../config/navigationItems";
 import AuthService from "../../../pages/api/authService";
 
-function AuthenticatedMenu({ onLogout }) {
-  const [avatarURL, setAvatarURL] = useState("/images/avatar.svg");
-
-  useEffect(() => {
-    const user = AuthService.getUser();
-    if (user.imageURL) {
-      setAvatarURL(user.imageURL);
-    }
-  }, []);
-
-  return (
-    <div className={styles.authenticatedMenu}>
-      <img className={styles.avatar} src={avatarURL} alt="avatar" />
-      <div className={styles.logoutButton} onClick={onLogout}>
-        <span className={styles.tabLabel}>Logout</span>
-      </div>
-    </div>
-  );
+function AuthenticatedMenu({ onLogout, avatarURL }) {
+	return (
+		<div className={styles.authenticatedMenu}>
+			<img className={styles.avatar} src={avatarURL || "/images/avatar.svg"} alt="avatar"/>
+			<div className={styles.logoutButton} onClick={onLogout}>
+				<span className={styles.tabLabel}>Logout</span>
+			</div>
+		</div>
+	);
 }
 
 function AnonymousMenu() {
-  return (
-    <div className={styles.anonymousMenu}>
-      <Link href="/login" passHref>
-        <LongButton value="Prijava" type="border" />
-      </Link>
-      <Link href="/registration" passHref>
-        <LongButton value="Registracija" type="filled" />
-      </Link>
-    </div>
-  );
+	return (
+		<div className={styles.anonymousMenu}>
+			<Link href="/login" passHref>
+				<LongButton value="Prijava" type="border"/>
+			</Link>
+			<Link href="/registration" passHref>
+				<LongButton value="Registracija" type="filled"/>
+			</Link>
+		</div>
+	);
 }
 
-export const DesktopNavigation = ({ onLogout }) => {
-  function MainMenu() {
-    return (
-      <div className={styles.middleMenu}>
-        {navigationItems.map((tab, index) => (
-          <div key={index} className={styles.tabItem}>
-            <Link href={`/${tab.pathname}`} passHref>
-              <span className={styles.tabLabel}>{tab.name}</span>
-            </Link>
-          </div>
-        ))}
-      </div>
-    );
-  }
+function MainMenu() {
+	return (
+		<div className={styles.middleMenu}>
+			{navigationItems.map((tab, index) => (
+				<div key={index} className={styles.tabItem}>
+					<Link href={`/${tab.pathname}`} passHref>
+						<span className={styles.tabLabel}>{tab.name}</span>
+					</Link>
+				</div>
+			))}
+		</div>
+	);
+}
 
-  return (
-    <nav className={styles.desktopNavigation}>
-      <LogoButton />
-      <MainMenu />
-      {onLogout ? <AuthenticatedMenu onLogout={onLogout} /> : <AnonymousMenu />}
-    </nav>
-  );
+export const DesktopNavigation = ({ onLogout, isAuthenticated }) => {
+	const [avatarURL, setAvatarURL] = useState("/images/avatar.svg");
+	const [isLoadingUser, setIsLoadingUser] = useState(true);
+
+	useEffect(() => {
+		if (isLoadingUser) {
+			const user = AuthService.getUser();
+			setAvatarURL(user?.imageURL);
+			setIsLoadingUser(false);
+		}
+	}, [isLoadingUser]);
+
+	return (
+		<nav className={styles.desktopNavigation}>
+			<LogoButton/>
+			<MainMenu/>
+			{!isLoadingUser && (
+				isAuthenticated ? <AuthenticatedMenu onLogout={onLogout} avatarURL={avatarURL}/> : <AnonymousMenu/>
+			)}
+		</nav>
+	);
 };
--- frontend/src/components/header/mobile-navigation/mobile.navigation.jsx
+++ frontend/src/components/header/mobile-navigation/mobile.navigation.jsx
@@ -6,70 +6,70 @@ import styles from "./mobile.navigation.module.scss";
 import { navigationItems } from "../../../config/navigationItems";
 import { LogoButton } from "../logo-button/logo.button";
 
-function MobileMenu({ onItemClick, onLogout }) {
-  const logout = useCallback(async () => {
-    onLogout();
-    onItemClick();
-  }, [onItemClick, onLogout]);
+function MobileMenu({ onItemClick, onLogout, isAuthenticated }) {
+	const logout = useCallback(async () => {
+		onLogout();
+		onItemClick();
+	}, [onItemClick, onLogout]);
 
-  function MenuItems() {
-    let menuItems = [...navigationItems];
-    if (!onLogout) {
-      menuItems = menuItems.concat([
-        {
-          name: "Login",
-          pathname: "/login",
-          active: false,
-        },
-        {
-          name: "Register",
-          pathname: "/registration",
-          active: false,
-        },
-      ]);
-    }
+	function MenuItems() {
+		let menuItems = [...navigationItems];
+		if (!isAuthenticated) {
+			menuItems = menuItems.concat([
+				{
+					name: "Login",
+					pathname: "/login",
+					active: false,
+				},
+				{
+					name: "Register",
+					pathname: "/registration",
+					active: false,
+				},
+			]);
+		}
 
-    const menuItemsJSX = menuItems.map((tab, index) => (
-      <Link key={index} href={`/${tab.pathname}`} passHref>
-        <div className={styles.tabItem} onClick={onItemClick}>
-          <span className={styles.tabLabel}>{tab.name}</span>
-        </div>
-      </Link>
-    ));
+		const menuItemsJSX = menuItems.map((tab, index) => (
+			<Link key={index} href={`/${tab.pathname}`} passHref>
+				<div className={styles.tabItem} onClick={onItemClick}>
+					<span className={styles.tabLabel}>{tab.name}</span>
+				</div>
+			</Link>
+		));
 
-    if (onLogout) {
-      menuItemsJSX.push(
-        <div key={menuItems.length} className={styles.tabItem} onClick={logout}>
-          <span className={styles.tabLabel}>Logout</span>
-        </div>
-      );
-    }
+		if (isAuthenticated) {
+			menuItemsJSX.push(
+				<div key={menuItems.length} className={styles.tabItem} onClick={logout}>
+					<span className={styles.tabLabel}>Logout</span>
+				</div>
+			);
+		}
 
-    return menuItemsJSX;
-  }
+		return menuItemsJSX;
+	}
 
-  return (
-    <div className={styles.mobileMenu}>
-      <MenuItems />
-    </div>
-  );
+	return (
+		<div className={styles.mobileMenu}>
+			<MenuItems/>
+		</div>
+	);
 }
 
-export const MobileNavigation = ({ isOpened, onToggle, onLogout }) => {
-  return (
-    <nav className={styles.mobileNavigation}>
-      <LogoButton />
-      {!isOpened && (
-        <div className={styles.openMobileMenuButton} onClick={onToggle}>
-          <FontAwesomeIcon icon={faBars} />
-        </div>
-      )}
-      {isOpened && (
-        <div className={styles.closeMobileMenuButton} onClick={onToggle}>
-          <FontAwesomeIcon icon={faXmark} />
-        </div>
-      )}
-      {isOpened && <MobileMenu onItemClick={onToggle} onLogout={onLogout} />}
-    </nav>
-  );
+export const MobileNavigation = ({ isOpened, onToggle, onLogout, isAuthenticated }) => {
+	return (
+		<nav className={styles.mobileNavigation}>
+			<LogoButton/>
+			{!isOpened && (
+				<div className={styles.openMobileMenuButton} onClick={onToggle}>
+					<FontAwesomeIcon icon={faBars}/>
+				</div>
+			)}
+			{isOpened && (
+				<div className={styles.closeMobileMenuButton} onClick={onToggle}>
+					<FontAwesomeIcon icon={faXmark}/>
+				</div>
+			)}
+			{isOpened && <MobileMenu onItemClick={onToggle} onLogout={onLogout} isAuthenticated={isAuthenticated}/>}
+		</nav>
+	);
 };
