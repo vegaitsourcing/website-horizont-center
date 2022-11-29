ce3ce9a77838541ba84140cae0a8bdad5dd2ef4a
Milos Roknic <roknic.milos.994@gmail.com>
2022-11-29 23:00:52 +0100

    Update Contact page

--- frontend/src/components/contact-page-content/contact.page.content.jsx
+++ frontend/src/components/contact-page-content/contact.page.content.jsx
@@ -7,63 +7,16 @@ export const ContactPageContent = () => {
 		<div className={styles.contactPageContent}>
 			<PageHeader
 				withBackground
-				title={"Kontakt"}
-				text={
-					"Felis lectus tortor massa a eget viverra integer faucibus adipiscing. " +
-					"Faucibus nunc, auctor arcu magna cursus."
-				}
+				title={"Povežimo se"}
 			/>
 			<SectionWithImage
 				imageSrc="/images/static.4.jpg"
-				title="Adipiscing platea aliquet"
 				paragraph={
-					`Ornare pulvinar ullamcorper in non elit leo, duis cursus. Adipiscing non faucibus molestie in tincidunt
-					ridiculus senectus. Molestie nunc et sed ut id urna, lectus nullaringilla arcu elit nisi imperdiet integer
-					tortor semper venenatis. Bibendum sagittis pellentesque dignissim nunc.`
-				}
-				paragraphItems={[
-					"E-mail adresa: caregivers@example.com",
-					"Telefon 1: +381 064 111 222",
-					"Telefon 2: +381 064 333 444",
-				]}
-			/>
-			<CenteredSection
-				title="Adipiscing platea aliquet"
-				paragraph={
-					`Ornare pulvinar ullamcorper in non elit leo, duis cursus. Adipiscing non faucibus molestie in tincidunt
-					ridiculus senectus. Molestie nunc et sed ut id urna, lectus nullaringilla arcu elit nisi imperdiet integer
-					tortor semper venenatis. Bibendum sagittis pellentesque dignissim nunc.`
-				}
-				buttonHref="/"
-			/>
-			<StepsSection
-				withBackground
-				withStepNumbers
-				title="Sekcija sa koracima"
-				description="Opis sekcije sa koracima, opis sekcije sa koracima, opis sekcije sa koracima, opis sekcije sa koracima"
-				steps={[
-					{
-						title: "Prvi korak",
-						text: "Opis prvog koraka. Opis prvog koraka. Opis prvog koraka. Opis prvog koraka.",
-					},
-					{
-						title: "Drugi korak",
-						text: "Opis drugog koraka. Opis drugog koraka. Opis drugog koraka. Opis drugog koraka. Opis drugog koraka. Opis drugog koraka.",
-					},
-					{
-						title: "Treći korak",
-						text: "Opis trećeg koraka. Opis trećeg koraka. Opis trećeg koraka. Opis trećeg koraka.",
-					},
-				]}
-			/>
-			<SectionWithImage
-				hasImageFirst
-				imageSrc="/images/static.6.jpg"
-				title="Adipiscing platea aliquet"
-				paragraph={
-					`Ornare pulvinar ullamcorper in non elit leo, duis cursus. Adipiscing non faucibus molestie in tincidunt
-					ridiculus senectus. Molestie nunc et sed ut id urna, lectus nullaringilla arcu elit nisi imperdiet integer
-					tortor semper venenatis. Bibendum sagittis pellentesque dignissim nunc.`
+					<>
+						<p>Centar HORIZONT 21</p>
+						<p>Novi Sad</p>
+						<p>zajednica@negovatelji.rs</p>
+					</>
 				}
 			/>
 		</div>
