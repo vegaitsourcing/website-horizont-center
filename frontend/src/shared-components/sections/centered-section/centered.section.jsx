f97364107e3aaeae8379314f89459a69021a8d5a
Milos Roknic <roknic.milos.994@gmail.com>
2022-11-30 22:46:06 +0100

    Add first 5 sections to Homepage

--- frontend/src/shared-components/sections/centered-section/centered.section.jsx
+++ frontend/src/shared-components/sections/centered-section/centered.section.jsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 import { LongButton } from "../../long-button/long.button";
 import React from "react";
 
-export function CenteredSection({
+export function CenteredSection ({
 	title,
 	paragraph,
 	buttonHref,
 	buttonLabel = "Saznaj više",
-	withBackground = false
+	withBackground = false,
 }) {
 	const sectionClasses = [styles.centeredSection, withBackground ? styles.lightBlueBackground : ""].join(" ");
 
@@ -16,13 +16,15 @@ export function CenteredSection({
 		<section className={sectionClasses}>
 			<h3 className={styles.h2}>{title}</h3>
 			<p className={styles.description}>{paragraph}</p>
-			<div className={styles.buttonWrapper}>
-				<Link href={buttonHref}>
-					<div>
-						<LongButton value={buttonLabel}/>
-					</div>
-				</Link>
-			</div>
+			{buttonHref && (
+				<div className={styles.buttonWrapper}>
+					<Link href={buttonHref}>
+						<div>
+							<LongButton value={buttonLabel}/>
+						</div>
+					</Link>
+				</div>
+			)}
 		</section>
 	);
 }
