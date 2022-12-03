80a4db2c196308fa00381c897e73ea916cb3619d
M1K1B <miki.balabanovic111@gmail.com>
2022-12-03 20:59:40 +0100

    Populating footer with data

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -5,46 +5,47 @@ import Link from "next/link";
 import { navigationItems } from "../../config/navigationItems";
 
 export const Footer = ({ isHidden }) => {
-	return (
-		<footer className={[styles.footer, isHidden ? styles.hidden : ""].join(" ")}>
-			<section className={styles.logoSection}>
-				<img src="/images/logo.white.svg" alt="Negovatelji logo" className={styles.logo}/>
-			</section>
-			<section className={styles.mainSection}>
-				<div className={styles.leftSide}>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae pariatur mollitia voluptatibus ab quis?
-						Deserunt eum libero qui dolores.
-					</p>
-					<div className={styles.socialMediaButtons}>
-						<a href="#" target="_blank">
-							<Image src="/images/facebookIconWhite.svg" width="16.11" height="30" alt="link to our facebook"/>
-						</a>
-						<a href="#" target="_blank">
-							<Image src="/images/twitterIconWhite.svg" width="30" height="24" alt="link to our twitter"/>
-						</a>
-						<a href="#" target="_blank">
-							<Image src="/images/linkedinIconWhite.svg" width="26" height="26" alt="link to our linkedin"/>
-						</a>
-					</div>
-				</div>
-				<div className={styles.rightSide}>
-					{navigationItems.map(item => (
-						<Link key={item.pathname} href={`/${item.pathname}`}>
-							<p className={styles.linkItem}>{item.name}</p>
-						</Link>
-					))}
-				</div>
-			</section>
-			<section className={styles.copyrightsSection}>
-				<p className={styles.leftSide}>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quia nisi ducimus accusamus vitae qui a debitis
-					inventore?
-				</p>
-				<p className={styles.rightSide}>
-					LOREM IPSUM
-				</p>
-			</section>
-		</footer>
-	);
+  return (
+    <footer className={[styles.footer, isHidden ? styles.hidden : ""].join(" ")}>
+      <section className={styles.logoSection}>
+        <img src="/images/logo.white.svg" alt="Negovatelji logo" className={styles.logo} />
+      </section>
+      <section className={styles.mainSection}>
+        <div className={styles.leftSide}>
+          <p>
+            Društvo koje ceni, podržava i osnažuje porodične negovatelje željena je budućnost Centra HORIZONT 21 i
+            platforme NEGOVATELJI.RS kreirane uz podršku podršku VEGAT IT kompanije i projekta Code For Couse.
+          </p>
+          <div className={styles.socialMediaButtons}>
+            <a href="https://www.facebook.com/groups/270285275225942/" target="_blank">
+              <Image src="/images/facebookIconWhite.svg" width="16.11" height="30" alt="link to our facebook" />
+            </a>
+            <a href="https://www.youtube.com/channel/UCS5gh6FEudKxWeUPIzL_Kog" target="_blank">
+              <Image src="/images/youtubeWhite.svg" width="30" height="30" alt="link to our youtube" />
+            </a>
+            <a href="https://instagram.com/negovatelji?igshid=MWI4MTIyMDE=" target="_blank">
+              <Image src="/images/instagramIconWhite.svg" width="25" height="30" alt="link to our instagram" />
+            </a>
+          </div>
+        </div>
+        <div className={styles.rightSide}>
+          {navigationItems.map((item) => (
+            <Link key={item.pathname} href={`/${item.pathname}`}>
+              <p className={styles.linkItem}>{item.name}</p>
+            </Link>
+          ))}
+          <Link key={"PrivacyPolicy"} href={"/documents/privacyPolicy.pdf"}>
+            <p className={styles.linkItem}>Politika privatnosti</p>
+          </Link>
+          <Link key={"TermsAndConditions"} href={"/documents/termsAndConditions.pdf"}>
+            <p className={styles.linkItem}>Uslovi korišćena sajta</p>
+          </Link>
+        </div>
+      </section>
+      <section className={styles.copyrightsSection}>
+        <p className={styles.leftSide}>© Copyright 2022 Negovatelji.rs - Sva prava zadržana</p>
+        <p className={styles.rightSide}>Designed by VEGA IT</p>
+      </section>
+    </footer>
+  );
 };
